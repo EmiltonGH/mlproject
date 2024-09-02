6165e9561735c97d6010edc5b8b69de1bbfbc1f1
Emilton Soosaippillai <emiltons@icloud.com>
2024-09-02 18:12:44 +0100

    Data transformation using pipelines

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,10 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+import dill
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -19,7 +23,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook/StudentsPerformance.csv')
+            df = pd.read_csv('notebook/stud.csv')
             logging.info('Read the dataset as dataframe')
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok = True)
             df.to_csv(self.ingestion_config.raw_data_path, index = False, header = True)
@@ -39,3 +43,9 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
         
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    DataTransformation = DataTransformation()
+    train_arr, test_arr, _ = DataTransformation.initiate_data_transformation(train_data, test_data)
